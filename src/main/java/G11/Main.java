c4cc2327a3cb49bb14973cedb881e01ff68ad64a
Cedric Jadin <cedric.jadin@hotmail.com>
2025-07-27 17:40:58 +1000

    Test JavaFX

--- src/main/java/G11/Main.java
+++ src/main/java/G11/Main.java
@@ -1,18 +1,26 @@
 package G11;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-        System.out.println("########### : Test Cedric OOSD G11");
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello world!");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 400, 200);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Maven Project");
+        primaryStage.show();
     }
 }
